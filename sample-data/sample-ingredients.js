bd47ab5bd6f5124e6e7b85147b1ce14ee1a98735
Devin Altobello <130494366+alto-fx@users.noreply.github.com>
2023-08-28 08:57:25 -0400

    Add sample data files

---
+++ sample-data/sample-ingredients.js
@@ -0,0 +1,24 @@
+const sampleIngredientsData = [
+    {
+      "id": 20081,
+      "name": "wheat flour",
+      "estimatedCostInCents": 142
+    },
+    {
+      "id": 1053,
+      "name": "cream",
+      "estimatedCostInCents": 645
+    },
+    {
+      "id": 11090,
+      "name": "broccoli crowns",
+      "estimatedCostInCents": 256
+    },
+    {
+      "id": 11821,
+      "name": "red sweet peppers",
+      "estimatedCostInCents": 1027
+    },
+  ];
+
+  export default sampleIngredientsData
