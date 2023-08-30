80a0af79d43f7cf20acc74992fc1268fe00f8e49
Devin Altobello <130494366+alto-fx@users.noreply.github.com>
2023-08-30 18:27:53 -0400

    Add tested calculateRecipeCost function

--- src/filter-recipes.js
+++ src/filter-recipes.js
@@ -48,41 +48,28 @@ const getIngredientsByRecipe = (recipeList, ingredientsList, name) => {
 //return array of total cost of each ingredient based on their amount
 //reduce()? - add all numbers up to get total cost
 
-const calculateRecipeCost = (recipeList, ingredientsList, name) => {
-  //find the recipe!
-  const recipeObject = recipeList.find(recipe => {
-    if (recipe.name === name) {
-      return recipe;
-    } else {
-      return false;
-    }
-  });
-  // console.log('recipeObject', recipeObject);
-  //get ingredient id from the recipe
+// const calculateRecipeCost = (recipe, ingredients) => {
+//   // console.log("RECIPE", recipe)
+//   return ingredients.reduce((sum, ingredient) => {
+//     // console.log(sum)
+//     // console.log(ingredient)
+//     console.log(recipe[0].ingredients)
+//     let ingredientQuantity = recipe[0].ingredients.find((dish) => {
+//       // console.log("DISH", dish)
+//       // console.log("Ing", ingredient)
+//       return dish.id ===  ingredient.id
+//     })
+//     console.log("INGED QUANTITY:, ", ingredientQuantity)
+//     sum += (ingredientQuantity.quantity.amount * ingredient.estimatedCostInCents)
+//     return sum
+//   }, 0)
+// }
 
-  let recipeIngredientId = recipeObject.ingredients.map(
-    ingredient => ingredient.id
-  );
-
-  let filteredIngredients = ingredientsList.filter(ingredient =>
-    recipeIngredientId.includes(ingredient.id)
-  );
-  console.log(filteredIngredients);
-
-  let ingredientCostInCents = filteredIngredients.map(
-    ingredient => ingredient.estimatedCostInCents
-  );
-  console.log('ingredientCostInCents', ingredientCostInCents);
-
-  let ingredientCostInDollars = ingredientCostInCents * 100;
-  console.log('ingredientCostInDollars', ingredientCostInDollars);
-};
-
-// const getIngredientsByRecipe = (recipeObj, ingredientsList) => {
-//   console.log("recipeObj:=====", recipeObj);
-
-//invoke filterByName()
-//error handling (sad path)
+const calculateRecipeCost = (recipe, ingredients) => {
+  const totalCost = recipe[0].ingredients.reduce((acc, {id, quantity: {amount}}) => 
+    acc + (amount / 100) * (ingredients.find(ingredient => ingredient.id === id).estimatedCostInCents), 0)
+  return totalCost.toFixed(2);
+}
 
 const getRecipeInstructions = (recipes, name) => {
   const targetRecipe = recipes.find(recipe => recipe.name === name);
