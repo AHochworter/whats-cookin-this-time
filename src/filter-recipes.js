c27fb03893251f10d6f87f30371febb942f12b54
Devin Altobello <130494366+alto-fx@users.noreply.github.com>
2023-08-31 09:20:12 -0400

    Refactor calculateRecipeCost

--- src/filter-recipes.js
+++ src/filter-recipes.js
@@ -27,49 +27,45 @@ const getIngredientsByRecipe = (recipeList, ingredientsList, name) => {
       return false;
     }
   });
-  // console.log(recipeObject);
-
-  let recipeIngredientId = recipeObject.ingredients.map(
+  const recipeIngredientId = recipeObject.ingredients.map(
     ingredient => ingredient.id
   );
 
-  let filteredIngredients = ingredientsList.filter(ingredient =>
+  const filteredIngredients = ingredientsList.filter(ingredient =>
     recipeIngredientId.includes(ingredient.id)
   );
-  // console.log(filteredIngredients);
-
-  let ingredientNames = filteredIngredients.map(ingredient => ingredient.name);
-
+  const ingredientNames = filteredIngredients.map(ingredient => ingredient.name);
   return ingredientNames;
 };
-//find ingredient in recipe - access ingredient id
-//get ingredient id that's sitting in the recipe
-//math for estimated cost
-//return array of total cost of each ingredient based on their amount
-//reduce()? - add all numbers up to get total cost
+
+
 
 // const calculateRecipeCost = (recipe, ingredients) => {
-//   // console.log("RECIPE", recipe)
-//   return ingredients.reduce((sum, ingredient) => {
-//     // console.log(sum)
-//     // console.log(ingredient)
-//     console.log(recipe[0].ingredients)
+//   const totalCost = recipe[0].ingredients.reduce((sum, ingredient) => {
 //     let ingredientQuantity = recipe[0].ingredients.find((dish) => {
-//       // console.log("DISH", dish)
-//       // console.log("Ing", ingredient)
 //       return dish.id ===  ingredient.id
 //     })
-//     console.log("INGED QUANTITY:, ", ingredientQuantity)
 //     sum += (ingredientQuantity.quantity.amount * ingredient.estimatedCostInCents)
 //     return sum
 //   }, 0)
 // }
 
 const calculateRecipeCost = (recipe, ingredients) => {
-  const totalCost = recipe[0].ingredients.reduce((acc, {id, quantity: {amount}}) => 
-    acc + (amount / 100) * (ingredients.find(ingredient => ingredient.id === id).estimatedCostInCents), 0)
+  const totalCost = recipe[0].ingredients.reduce((sum, ingredient) => {
+    // console.log("Ingred", ingredient)
+    const recipeIngredId = ingredient.id;
+    const recipeAmount = ingredient.quantity.amount;
+    const matchingIngredient = ingredients.find(ingredient => {
+      return  ingredient.id === recipeIngredId
+    });
+    const estimatedCostInCents = matchingIngredient.estimatedCostInCents;
+    return sum += (recipeAmount / 100) * estimatedCostInCents;
+  }, 0);
+
   return totalCost.toFixed(2);
 }
+
+
 
 const getRecipeInstructions = (recipes, name) => {
   const targetRecipe = recipes.find(recipe => recipe.name === name);
