e0ac5c341a18a4bdcbef0bfdbf949aa71721c475
Devin Altobello <130494366+alto-fx@users.noreply.github.com>
2023-08-31 21:31:51 -0400

    Add WIP renderRecipeDetails function

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -1,10 +1,12 @@
 //NOTE: Your DOM manipulation will occur in this file
 import ingredientsData from '../sample-data/sample-ingredients';
 import recipeData from '../sample-data/sample-recipes.js';
+import { filterByTag, filterByName, calculateRecipeCost, getRecipeInstructions } from '../src/filter-recipes'
 // import userData from '../sample-data/sample-users';
 
 //Query Selectors Here👇
 const recipeContainer = document.querySelector('.recipe-container');
+const individualRecipeDetails = document.querySelector('.individual-recipe-view');
 
 //Buttons
 
@@ -28,4 +30,33 @@ const renderRecipeCards = recipeList => {
 //update each recipe card
 //view all recipes
 
-export { renderRecipeCards };
+const renderRecipeDetails = (event) =>{
+  console.log("hellllo")
+  individualRecipeDetails.innerHTML += ' '
+  const chosenRecipe = filterByName(recipeData, name)
+  const recipeCost = calculateRecipeCost(recipeData, ingredientsData)
+  const ingredientDetails = getIngredientsByRecipe(recipeData, ingredientsData, name)
+  const instructions = getRecipeInstructions(recipeData, ingredientsData, name)
+
+
+  individualRecipeDetails.innerHTML += `
+  <div class="recipe-name-wrapper">
+    <h3 class="recipe-name">${chosenRecipe[0].name}</h3>
+  </div>
+  <div class="recipe-image-wrapper">
+  <img
+          src="${chosenRecipe[0].image}" alt="${chosenRecipe[0].name}" class="recipe-image">
+  </div>
+  <div class="recipe-ingredients-wrapper">
+    <h3 class="individual-recipe-headings">Ingredients</h3>
+    <div class="recipe-ingredients-list">${ingredientDetails.ingredients}</div>
+    <p class="recipe-cost">This recipe costs $${recipeCost} to make.</p>
+  </div>
+  <div class="recipe-instructions-wrapper">
+    <h3 class="individual-recipe-headings">Instructions</h3>
+    <div class="recipe-instructions-list">${instructions[0].instructions}</div>
+  </div>`
+}
+
+
+export { renderRecipeCards, renderRecipeDetails };
