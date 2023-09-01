0ac86ac47eba70edc1f3252fc09d7efee2e39ae2
Devin Altobello <130494366+alto-fx@users.noreply.github.com>
2023-09-01 18:47:12 -0400

    Update data import path

--- src/scripts.js
+++ src/scripts.js
@@ -5,7 +5,7 @@ import apiCalls from './apiCalls';
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 // import './images/turing-logo.png'
 import ingredientsData from './data/ingredients.js';
-import recipeData from './data/recipes.js';
+import recipeData from './data/recipes';
 // import userData from './data/users.js';
 
 import { renderRecipeCards } from '../src/domUpdates';
