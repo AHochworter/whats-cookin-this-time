d65765b629254cf8c9abdee89ee697ca6172d410
Ann Hochworter <ann.hochworter@gmail.com>
2023-09-02 10:22:51 -0600

    Resolve merge conflicts in domUpdates.js

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -2,7 +2,6 @@
 
 import ingredientsData from './data/ingredients';
 import recipeData from './data/recipes';
-<<<<<<< HEAD
 import {
   filterByTag,
   filterByName,
@@ -12,17 +11,6 @@ import {
   findRecipe,
   formatInstructions,
 } from '../src/filter-recipes';
-=======
-import { filterByTag, filterByName, calculateRecipeCost, getIngredientsByRecipe, getRecipeInstructions, findRecipe, formatInstructions } from '../src/filter-recipes'
-
-
-// import recipeData from '../sample-data/sample-recipes.js';
-
-
-// import { filterByTag } from './filter-recipes';
-
-
->>>>>>> bc389c98ac348d4686f120c8133d8a1b5c318761
 // import userData from '../sample-data/sample-users';
 
 //Query Selectors Here👇
@@ -171,17 +159,8 @@ const renderSelectTagOptions = tagData => {
     dropDownMenu.innerHTML += `
     <option value='${tag}'>${tag}</option>
     `;
-<<<<<<< HEAD
   });
 };
-=======
-  }); 
-}
-
-
-export { renderSearchResults, renderRecipeCardsByTag, renderSelectTagOptions, selectButton, dropDownMenu };
-
->>>>>>> bc389c98ac348d4686f120c8133d8a1b5c318761
 
 export {
   // renderRecipeCards,
