ff017406d0af22493cc60cefbfa18892fd57054a
Ann Hochworter <ann.hochworter@gmail.com>
2023-09-03 16:11:29 -0600

    Add fetch-api calls

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,4 +1,31 @@
 // Your fetch requests will live here!
+export const getUsers = () => {
+  return fetch('https://what-s-cookin-starter-kit.herokuapp.com/api/v1/users')
+    .then(response => response.json())
+    .then(data => {
+      return data;
+    })
+    .catch(error => console.log('error'));
+};
 
+export const getRecipes = () => {
+  return fetch('https://what-s-cookin-starter-kit.herokuapp.com/api/v1/recipes')
+    .then(response => response.json())
+    .then(data => {
+      return data;
+    })
+    .catch(error => console.log('error'));
+};
 
-console.log('I will be a fetch request!')
+export const getIngredients = () => {
+  return fetch(
+    'https://what-s-cookin-starter-kit.herokuapp.com/api/v1/ingredients'
+  )
+    .then(response => response.json())
+    .then(data => {
+      return data;
+    })
+    .catch(error => console.log('error'));
+};
+
+console.log('I will be a fetch request!');
