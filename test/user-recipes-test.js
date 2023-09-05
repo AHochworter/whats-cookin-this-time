3a83039642d7a9290541daa816967ed99eb03317
Nicole Ngoc Gia Lam <nicolelam8891@gmail.com>
2023-09-05 16:17:04 -0600

    Add beforeEach to our tests

--- test/user-recipes-test.js
+++ test/user-recipes-test.js
@@ -11,29 +11,15 @@ import { getRandomUser, deleteRecipe, saveRecipe, savedRecipes } from '../src/us
 import recipeData from '../sample-data/sample-recipes';
 
 describe('getRandomUser', () => {
-  it('should be a function', () => {
-    expect(getRandomUser).to.be.a('function'); 
-  });
-
-  it('should return a user from user list', () => {
-    const users = [
+  let users, currentUser, filtered1, myFirstFavoriteRecipe
+  beforeEach(() => {
+    users = [
       { id: 1, name: 'Sadye Welch' },
       { id: 2, name: 'Jordyn West' },
       { id: 3, name: 'Willie Eichmann' },
-    ];
-    const randomUser = getRandomUser(users);
-
-    expect(users).to.include(randomUser);
-  });
-});
-
-describe('Save recipes', () => {
-  it('should be a function', () => {
-    expect(saveRecipe).to.be.a('function');
-  });
-
-  it('should save a recipe to the savedRecipe array', () => {
-    const currentUser = {
+    ];  
+  
+    currentUser = {
       name: "Saige O'Kon",
       id: 1,
       pantry: [
@@ -42,61 +28,86 @@ describe('Save recipes', () => {
           amount: 4,
         }
       ],
-      recipesToCook: [],
-    } 
-    const filtered1 = filterByName(recipeData, 'Maple Dijon Apple Cider Grilled Pork Chops');
-      const myFirstFavoriteRecipe = saveRecipe(recipeData, filtered1[0].name, currentUser); 
-      expect(filtered1.length).to.equal(1);
-      expect(currentUser.recipesToCook.length).to.equal(1);
-      expect(myFirstFavoriteRecipe).to.deep.equal([
+      recipesToCook: []
+    }
+
+    filtered1 = filterByName(recipeData, 'Maple Dijon Apple Cider Grilled Pork Chops');
+
+    myFirstFavoriteRecipe = saveRecipe(recipeData, filtered1[0].name, currentUser); 
+  });
+
+  it('should be a function', () => {
+    expect(getRandomUser).to.be.a('function'); 
+  });
+
+  it('should return a user from user list', () => {
+    const randomUser = getRandomUser(users);
+    expect(users).to.include(randomUser);
+  });
+});
+
+describe('Save recipes', () => {
+  let currentUser, filtered1, myFirstFavoriteRecipe
+  beforeEach(() => {
+    currentUser = {
+      name: "Saige O'Kon",
+      id: 1,
+      pantry: [
         {
-          id: 678353,
-          image: 'https://spoonacular.com/recipeImages/678353-556x370.jpg',
-          ingredients: [
-            { id: 1009016, quantity: { amount: 1.5, unit: 'cups' } },
-            { id: 9003, quantity: { amount: 2, unit: '' } },
-            { id: 20027, quantity: { amount: 1, unit: 'tablespoon' } },
-            { id: 1002046, quantity: { amount: 1, unit: 'tablespoon' } },
-            { id: 11215, quantity: { amount: 1, unit: 'clove' } },
-            { id: 1012046, quantity: { amount: 1, unit: 'tablespoon' } },
-            { id: 19911, quantity: { amount: 0.25, unit: 'cup' } },
-            { id: 16112, quantity: { amount: 1, unit: 'tablespoon' } },
-            { id: 10010062, quantity: { amount: 24, unit: 'ounce' } },
-            { id: 1102047, quantity: { amount: 4, unit: 'servings' } },
-            { id: 16124, quantity: { amount: 1, unit: 'tablespoon' } },
-            { id: 1016168, quantity: { amount: 1, unit: 'tablespoon' } },
-          ],
-          instructions: [
-            {
-              instruction:
-                'Season the pork chops with salt and pepper and grill or pan fry over medium high heat until cooked, about 3-5 minutes per side. (If grilling, baste the chops in the maple dijon apple cider sauce as you grill.)Meanwhile, mix the remaining ingredients except the apple slices, bring to a simmer and cook until the sauce thickens, about 2-5 minutes.Grill or saute the apple slices until just tender but still crisp.Toss the pork chops and apple slices in the maple dijon apple cider sauce and enjoy!',
-              number: 1,
-            },
-          ],
-          name: 'Maple Dijon Apple Cider Grilled Pork Chops',
-          tags: ['lunch', 'main course', 'main dish', 'dinner'],
-        },
-      ]);
-    });
+          ingredient: 11297,
+          amount: 4,
+        }
+      ],
+      recipesToCook: []
+    }
+    filtered1 = filterByName(recipeData, 'Maple Dijon Apple Cider Grilled Pork Chops');
+
+    myFirstFavoriteRecipe = saveRecipe(recipeData, filtered1[0].name, currentUser); 
+  });
+
+  it('should be a function', () => {
+    expect(saveRecipe).to.be.a('function');
+  });
+
+  it('should save a recipe to the savedRecipe array', () => {
+    expect(filtered1.length).to.equal(1);
+    expect(currentUser.recipesToCook.length).to.equal(1);
+    expect(myFirstFavoriteRecipe).to.deep.equal([
+      {
+        id: 678353,
+        image: 'https://spoonacular.com/recipeImages/678353-556x370.jpg',
+        ingredients: [
+          { id: 1009016, quantity: { amount: 1.5, unit: 'cups' } },
+          { id: 9003, quantity: { amount: 2, unit: '' } },
+          { id: 20027, quantity: { amount: 1, unit: 'tablespoon' } },
+          { id: 1002046, quantity: { amount: 1, unit: 'tablespoon' } },
+          { id: 11215, quantity: { amount: 1, unit: 'clove' } },
+          { id: 1012046, quantity: { amount: 1, unit: 'tablespoon' } },
+          { id: 19911, quantity: { amount: 0.25, unit: 'cup' } },
+          { id: 16112, quantity: { amount: 1, unit: 'tablespoon' } },
+          { id: 10010062, quantity: { amount: 24, unit: 'ounce' } },
+          { id: 1102047, quantity: { amount: 4, unit: 'servings' } },
+          { id: 16124, quantity: { amount: 1, unit: 'tablespoon' } },
+          { id: 1016168, quantity: { amount: 1, unit: 'tablespoon' } },
+        ],
+        instructions: [
+          {
+            instruction:
+              'Season the pork chops with salt and pepper and grill or pan fry over medium high heat until cooked, about 3-5 minutes per side. (If grilling, baste the chops in the maple dijon apple cider sauce as you grill.)Meanwhile, mix the remaining ingredients except the apple slices, bring to a simmer and cook until the sauce thickens, about 2-5 minutes.Grill or saute the apple slices until just tender but still crisp.Toss the pork chops and apple slices in the maple dijon apple cider sauce and enjoy!',
+            number: 1,
+          },
+        ],
+        name: 'Maple Dijon Apple Cider Grilled Pork Chops',
+        tags: ['lunch', 'main course', 'main dish', 'dinner'],
+      },
+    ]);
+  });
     
     it('should be a function', () => {
-    expect(deleteRecipe).to.be.a('function');
+      expect(deleteRecipe).to.be.a('function');
     });
 
     it('should delete a recipe from the saved recipe array', () => {
-      const currentUser = {
-        name: "Saige O'Kon",
-        id: 1,
-        pantry: [
-          {
-            ingredient: 11297,
-            amount: 4,
-          }
-        ],
-        recipesToCook: [],
-      } 
-    const filtered1 = filterByName(recipeData, 'Maple Dijon Apple Cider Grilled Pork Chops');
-    const myFirstFavoriteRecipe = saveRecipe(recipeData, filtered1[0].name, currentUser); 
       const deleteARecipe = deleteRecipe(savedRecipes, filtered1.name);
       const filtered2 = filterByName(recipeData, 'Sesame Cookies');
       const mySecondFavoriteRecipe = saveRecipe(recipeData, filtered2[0].name, currentUser);
