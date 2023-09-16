cbc49a3efb9d6c43f5583d211febd06df9c3ddfe
Ann Hochworter <ann.hochworter@gmail.com>
2023-09-16 15:01:44 -0600

    Adjust saveRecipes to POST recipeID

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -18,9 +18,7 @@ export const getRecipes = () => {
 };
 
 export const getIngredients = () => {
-  return fetch(
-    '	http://localhost:3001/api/v1/ingredients'
-  )
+  return fetch('	http://localhost:3001/api/v1/ingredients')
     .then(response => response.json())
     .then(data => {
       return data;
@@ -28,26 +26,47 @@ export const getIngredients = () => {
     .catch(error => console.log('error'));
 };
 
-
 export const postRecipe = (recipeID, userID) => {
   // console.log(recipeID)
   // console.log(userID)
-  const postObject = { userID: userID, recipeID: recipeID } 
+  const postObject = { userID: userID, recipeID: recipeID };
   //put the postObject into the body. This was from the spec
-    
- return fetch(`http://localhost:3001/api/v1/usersRecipes`, {
-      method: "POST",
+
+  return (
+    fetch(`http://localhost:3001/api/v1/usersRecipes`, {
+      method: 'POST',
       body: JSON.stringify(postObject),
       headers: {
-          "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
+    })
+      .then(response => response.json())
+      // .then((data) =>  {
+      //   console.log(data) // iterate over recipe data match ID of recipe user has clicked on
+      // })
+      .catch(error => console.log(error))
+  );
+};
+
+export const updateUsers = (currentUser, savedRecipe) => {
+  const currentRecipe = currentUser['recipesToCook'].find(
+    item => item === savedRecipe
+  );
+  if (currentRecipe) {
+    console.log('Duplicate recipeID found. Cannot add the same recipe again.');
+    return Promise.reject('Duplicate recipeID');
+  }
+  const promise = fetch('http://localhost:3001/api/v1/usersRecipes', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      userID: currentUser['id'],
+      recipeID: savedRecipe['id'],
+    }),
   })
-  .then((response) => response.json())
-  // .then((data) =>  {
-  //   console.log(data) // iterate over recipe data match ID of recipe user has clicked on
-  // })
-  .catch((error) => console.log(error))
-}
-
-
-// populate savedRecipes with 
+    .then(response => response.json())
+    .catch(err => console.error(`You got an ${err}`));
+  return promise;
+};
